942f943860ef2faa2fa40f9ed68ca6fcfb78841b
AdrianTejada <atejada@my.bcit.ca>
2023-05-16 22:13:03 -0700

    Added sections, snippets, and templates to config

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,7 +1,11 @@
 /** @type {import('tailwindcss').Config} */
 module.exports = {
   content: [
-    './layout/*.liquid'
+    './layout/*.liquid',
+    './sections/*.liquid',
+    './snippets/*.liquid',
+    './templates/customers/*.liquid',
+    './templates/*.liquid'
   ],
   theme: {
     extend: {},
