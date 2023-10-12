3d052b6f176c9dc9bbeacd3113355dd67d77fdb9
cyyryan <cyyryan0623@gmail.com>
2023-10-11 22:01:56 -0700

    fix

---
+++ tailwind.config.js
@@ -0,0 +1,15 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: [
+    './layout/*.liquid',
+    './sections/*.liquid',
+    './snippets/*.liquid',
+    './snippets/customers/*.liquid',
+    './templates/*.liquid',
+  ],
+  theme: {
+    extend: {},
+  },
+  plugins: [],
+}
+
